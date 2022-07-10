29a97897513083df8cccffb57227492b06e5e6e5
Juan Maurente <juanmaurente@gmail.com>
2022-07-10 23:56:13 +1000

    Nav icon insertion
    not working

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ document.querySelectorAll('.projects__gallery img').forEach((image) => {
 document.querySelector('.projects__popup span').onclick = () => {
 	document.querySelector('.projects__popup').style.display = 'none';
 };
+
+function myFunction() {
+	var x = document.getElementById('navbar');
+	if (x.className === 'navbar') {
+		x.className += '--responsive';
+	} else {
+		x.className = 'navbar';
+	}
+}
