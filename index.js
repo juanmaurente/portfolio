970e88f23bcdfe8f8c4640dacef2f2b78a6df495
Juan Maurente <juanmaurente@gmail.com>
2022-07-11 00:07:25 +1000

    Title of project on popup

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ document.querySelectorAll('.projects__gallery img').forEach((image) => {
 		document.querySelector('.projects__popup').style.display = 'block';
 		document.querySelector('.projects__popup img').src =
 			image.getAttribute('src');
+		document.querySelector('.projects__popup h2').innerHTML =
+			image.getAttribute('alt');
 	};
 });
 
